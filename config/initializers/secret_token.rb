e29a33bb9492213baf210a79e45f778e55217ae3
Guy Zisman <guyzisman@gammado.com>
2013-03-01 10:24:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyProj::Application.config.secret_token = '1477d37b4719ea20486f2dd420da6508522290f5cd1f0baad43507f87be7445fd4a43bfce007f33da4e1e4d0ffdfcfb64c81c30bdd64dd2dc75a145e7bcae009'
